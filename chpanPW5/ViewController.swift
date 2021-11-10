d77e2e6d4f50e81abe0d5de59b2796800927f5f5
teddyzcxv <pan2691712848@gmail.com>
2021-11-10 17:25:01 +0300

    Initial Commit

---
+++ chpanPW5/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  chpanPW5
+//
+//  Created by Семён Кондаков on 10.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ chpanPW5UITests/chpanPW5UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  chpanPW5UITestsLaunchTests.swift
+//  chpanPW5UITests
+//
+//  Created by Семён Кондаков on 10.11.2021.
+//
+
+import XCTest
+
+class chpanPW5UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
